90b4a4b8cc3e602d5afee9995942913fdd2ed539
ghadow <oxracr@icloud.com>
2023-12-19 23:45:43 -0500

    Slider update
    js update

--- js/modal_slides.js
+++ js/modal_slides.js
@@ -1,26 +1,52 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+//..Current slider index
+var slideIndex = 0;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+//..Get the slides
+let slides = document.getElementsByClassName("headerSlides");
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+//..On start
+loop();
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+//..Timed rotation
+function loop() {
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(loop, 38000);
+}
+
+//..Update current slide
+function update() {
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+//..Check slide index before updating
+function verify() {
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  if (slideIndex < 1) { slideIndex = slides.length }
+}
+
+//..Previous button
+function decrement() {
+  slideIndex = (slideIndex - 1);
+  verify();
+  update();
+}
+
+//..Next button
+function increment() {
+  slideIndex = (slideIndex + 1);
+  verify();
+  update();
 }
